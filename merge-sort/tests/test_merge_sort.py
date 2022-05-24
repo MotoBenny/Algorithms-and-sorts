1f49e3a7f32de0235966c42150b4d0139ac07d6c
Benjamin <benj.james.carter@gmail.com>
2022-05-24 10:23:48 -0700

    tested more cases

--- merge-sort/tests/test_merge_sort.py
+++ merge-sort/tests/test_merge_sort.py
@@ -16,6 +16,7 @@ def test_long_sort():
     test_list = [4, 6, 13, 44, 2, 34, 265, 45, 34, 5, 234, 23, 4, 23, 2]
     actual = merge_sort(test_list)
     expected = [2, 2, 4, 4, 5, 6, 13, 23, 23, 34, 34, 44, 45, 234, 265]
+    assert actual == expected
 
 
 def test_negative_nums():
@@ -29,4 +30,11 @@ def test_float():
     test_list = [.34, .63, .36, .82]
     actual = merge_sort(test_list)
     expected = [.34, .36, .63, .82]
+    assert actual == expected
+
+
+def test_odd_length():
+    test_list = [5, 5, 12, 123, 4]
+    actual = merge_sort(test_list)
+    expected = [4, 5, 5, 12, 123]
     assert actual == expected
